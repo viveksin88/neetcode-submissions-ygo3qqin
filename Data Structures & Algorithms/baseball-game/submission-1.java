f3e1aed95b3f4ac41755b5ea1a5ccde42013e867
Vivek Singh <svivekkumar88@gmail.com>
2026-04-29 20:25:14 -0400

    Add: baseball-game - submission-1

---
+++ Data Structures & Algorithms/baseball-game/submission-1.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int calPoints(String[] operations) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (String op: operations) {
+            if (op.equals("+")) {
+                int top = stack.pop();
+                int newTop = top + stack.peek();
+                stack.push(top);
+                stack.push(newTop);
+            } else if (op.equals("D")) {
+                int newTop = 2 * stack.peek();
+                stack.push(newTop);
+            } else if (op.equals("C")) {
+                stack.pop();
+            } else {
+                stack.push(Integer.parseInt(op));
+            }
+        }
+
+        int sum = 0;
+        for(int score: stack) {
+            sum += score;
+        }
+
+        return sum;
+    }
+}
