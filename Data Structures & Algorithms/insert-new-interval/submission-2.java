860ebcc2b58db16dfd65cff72c34645a2353d624
Vivek Singh <svivekkumar88@gmail.com>
2026-08-23 20:48:06 -0400

    Add: insert-new-interval - submission-2

---
+++ Data Structures & Algorithms/insert-new-interval/submission-2.java
@@ -0,0 +1,42 @@
+/**
+ * Algorithm
+ *
+ * 1. Initialize an empty result list {@code res} and index {@code i = 0}.
+ * 2. Add all intervals that end before {@code newInterval} starts:
+ *      - while intervals[i].end < newInterval.start, append intervals[i] to res
+ * 3. Merge all intervals that overlap with {@code newInterval}:
+ *      - while intervals[i].start <= newInterval.end, update:
+ *          - newInterval.start = min(newInterval.start, intervals[i].start)
+ *          - newInterval.end   = max(newInterval.end, intervals[i].end)
+ * 4. Append the merged {@code newInterval} to {@code res}.
+ * 5. Append all remaining intervals (which must come after) to {@code res}.
+ * 6. Return {@code res}.
+ */
+
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> newIntervals = new ArrayList<>();
+        int n = intervals.length;
+        int i = 0;
+
+        while (i < n && intervals[i][1] < newInterval[0]) {
+            newIntervals.add(intervals[i]);
+            i++;
+        }
+
+        while (i < n && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+
+        newIntervals.add(newInterval);
+
+        while (i < n) {
+            newIntervals.add(intervals[i]);
+            i++;
+        }
+
+        return newIntervals.toArray(new int[newIntervals.size()][]);
+    }
+}
